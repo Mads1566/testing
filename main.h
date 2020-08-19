57c547d72649dfb6de74b201a7f60a786b176fe2
Mads <69863546+Mads1566@users.noreply.github.com>
2020-08-19 16:55:28 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+/*
+ * This is a simple Hello World in C++.
+ * Note the header structure and files.
+ * The only thing included in main.cpp is this main.h file.
+ * main.h contains all directives for main.cpp
+*/
+
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
